16cf3d014f7d78170756891078102684f6e05482
FrancianRod <francianprog.py@gmail.com>
2026-03-02 17:20:21 -0300

    Add files via upload
    Projeto IA

---
+++ ai_feedback_analytics/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+
+def get_engine():
+    engine = create_engine("sqlite:///feedback.db")
+    return engine
+
+def save_to_database(df, table_name="feedback_analysis"):
+    engine = get_engine()
+    df.to_sql(table_name, engine, if_exists='replace', index=False)
